8d17990182f48a3e12ad3ddfe140dff3c8c78403
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-01-10 19:02:19 +0200

    Update Afisare.h

--- Afisare.h
+++ Afisare.h
@@ -1,6 +1,7 @@
 #ifndef AFISARE_H_INCLUDED
 #define AFISARE_H_INCLUDED
 
+char alegereMiscare(char xTabla[], char player) ;
 char alegerePiesa();
 void instructiuni ();
 void tablaGoala(char xTabla[]);
